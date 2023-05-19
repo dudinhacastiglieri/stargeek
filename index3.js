60909dc82314b1c9c0ed2c802d0eb34f33867db9
duda <mariaedbcastiglieri@portalsesisp.org.br>
2023-05-19 07:49:35 -0300

    logica 180523

---
+++ index3.js
@@ -0,0 +1,22 @@
+const msg = document.querySelector(".mensagem");
+const formulario = document.getElementById("formulario");
+const email = document.getElementById("email");
+const senha = document.getElementById("senha");
+
+formulario.onsubmit = (evt) => { 
+    let dados = JSON.parse(localStorage.getItem("bd"));
+    dados.forEach((elemento) => {
+        if (elemento.email == email.value && elemento.senha == senha.value){
+            msg.innerHTML = "aguarde redirecionando..."
+            evt.preventdefault();
+            setTimeout(()=>{
+                window.location.assign("catalogo.html");
+            },2000);
+            return true;
+        } else {
+            msg.innerHTML = "usuario ou senhas incorretos!";
+            evt.preventdefault();
+            return null;
+        }
+    });
+}
